5cd5cd1a7127841bc3405a6c8cfc04154eb4b1fa
Cosmsos <cosmosappiah029@gmail.com>
2020-08-28 12:40:28 +0000

    added theming

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:Myshop/screens/home..dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(MyApp());
@@ -11,8 +12,17 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Shop App',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primaryColor: Colors.white,
+        accentColor: Color(0xff21B4A6),
+        canvasColor: Color(0xffF8F8F8),
         visualDensity: VisualDensity.adaptivePlatformDensity,
+        textTheme: TextTheme(
+          headline1: GoogleFonts.sourceSansPro(
+            fontSize: 22,
+            color: Color(0xff686868),
+            fontWeight: FontWeight.w400,
+          ),
+        ),
       ),
       home: Home(),
     );
