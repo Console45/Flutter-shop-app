3cb2ed924a7aa5f70833fd5455526ecb8fe1ab76
Cosmsos <cosmosappiah029@gmail.com>
2020-08-28 13:11:32 +0000

    added title,avatar and textfill to product overwiew screen

--- lib/screens/products_overwiew.dart
+++ lib/screens/products_overwiew.dart
@@ -8,8 +8,45 @@ class ProductsOverwiew extends StatefulWidget {
 class _ProductsOverwiewState extends State<ProductsOverwiew> {
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text('Products Overwiew'),
+    return Container(
+      padding: EdgeInsets.only(
+        top: MediaQuery.of(context).size.height * 0.08,
+        left: MediaQuery.of(context).size.width * 0.08,
+        right: MediaQuery.of(context).size.width * 0.08,
+      ),
+      child: Column(
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text(
+                'Discovery',
+                style: Theme.of(context).textTheme.headline1,
+              ),
+              CircleAvatar(
+                backgroundColor: Theme.of(context).accentColor,
+              )
+            ],
+          ),
+          SizedBox(
+            height: 40,
+          ),
+          TextField(
+            decoration: InputDecoration(
+              contentPadding: const EdgeInsets.all(17),
+              filled: true,
+              hintText: 'Search Plants',
+              hintStyle: TextStyle(color: Color(0xffCACACA), fontSize: 14),
+              prefixIcon: Icon(Icons.search),
+              fillColor: Theme.of(context).primaryColor,
+              border: UnderlineInputBorder(
+                borderSide: BorderSide.none,
+                borderRadius: BorderRadius.circular(30),
+              ),
+            ),
+          )
+        ],
+      ),
     );
   }
 }
